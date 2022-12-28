f1faa7ae2f3ea1f2e7546dedc6e70a929fd4a2aa
Megha.Wali <Megha.Wali@smithsnews.co.uk>
2022-12-28 15:05:24 +0530

    first commit

---
+++ client/script.js
@@ -0,0 +1,102 @@
+import bot from './assets/bot.svg';
+import user from './assets/user.svg';
+import ctepl from './assets/Logo.svg';
+
+
+const form = document.querySelector('form');
+const chatContainer = document.querySelector('#chat_container');
+
+let loadInterval;
+
+function loader(element) {
+  element.textContent = '';
+
+  loadInterval = setInterval(() => {
+    element.textContent += '.';
+
+    if (element.textContent === '...') {
+      element.textContent = '';
+    }
+  }, 300);
+}
+
+function typeText(element, text) {
+  let index = 0;
+  let intervalId = setInterval(() => {
+    if (index < text.length) {
+      element.innerHTML += text[index];
+      index++;
+    } else {
+      clearInterval(intervalId);
+    }
+  }, 20)
+}
+
+function generateUniqueId() {
+  const timeStamp = Date.now();
+  const randomNumber = Math.random();
+  const hexaDecimalString = randomNumber.toString(16);
+  return `id-${timeStamp}-${hexaDecimalString}`;
+}
+
+function chartStripe(isAi, value, unqueId) {
+  return (
+    `
+    <div class="wrapper ${isAi && 'ai'}">
+      <div class="chat" style="display:inline-flex">
+        <div class="profile">
+          <img style="height:50px;width:50px"  class="img-container"
+          src="${isAi ? ctepl : user}"
+          alt="${isAi ? ctepl : user}"
+          />
+        </div>
+        <div class="message" id="${unqueId}" >${value} </div>
+      </div>
+    </div>
+    `
+  );
+}
+
+const handleSubmit = async (e) => {
+  e.preventDefault();
+  const data = new FormData(form);
+  chatContainer.innerHTML += chartStripe(false, data.get('prompt'));
+  form.reset();
+
+
+  const uniqueId = generateUniqueId();
+  chatContainer.innerHTML += chartStripe(true, '', uniqueId);
+  chatContainer.scrollTop = chatContainer.scrollHeight;
+  const messageDiv = document.getElementById(uniqueId);
+  debugger;
+  loader(messageDiv);
+
+  const response = await fetch('http://localhost:5000', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      prompt: data.get('prompt')
+    })
+  });
+  clearInterval(loadInterval);
+
+  if (response.ok) {
+    const data = await response.json();
+    const parsedData = data.bot.trim();
+    typeText(messageDiv, parsedData);
+  } else {
+    const error = await response.text();
+    messageDiv.innerHTML += "Something went wrong";
+    alert(error);
+  }
+}
+
+form.addEventListener('submit', handleSubmit);
+
+form.addEventListener('keyup', (e) => {
+  if (e.keyCode === 13) {
+    handleSubmit(e);
+  }
+})
